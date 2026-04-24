7e605f22179d771c93f10fc495b0465247b6f9ad
Riphaty <riphatymkude008@gmail.com>
2026-04-25 02:54:34 +0300

    settings

--- alamo/settings.py
+++ alamo/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 import os
 import dj_database_url
 import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
