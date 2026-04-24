ad0f63acc933e0f98b563341d65ec228fe22f35a
Riphaty <riphatymkude008@gmail.com>
2026-04-25 00:37:11 +0300

    media

--- alamo/settings.py
+++ alamo/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
-
+    
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
