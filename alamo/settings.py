14f887502c9eb36f842f22924fd9cc770a27a7d4
Riphaty <riphatymkude008@gmail.com>
2026-04-25 14:55:25 +0300

    settings

--- alamo/settings.py
+++ alamo/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]                                   
 
@@ -146,7 +146,7 @@ STORAGES={
         "BACKEND":"whitenoise.storage.CompressedManifestStaticFilesStorage"
     }
 }
-MEDIA_URL= 'cloudinary://<498253397725823>:<UnczJM_9CTEPVHozpGGgIhJix7A>@dkewkxvie/'
+MEDIA_URL= '/media'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Default primary key field type
