6fa14c5ac3bb839c5946513a03363744421cc3b5
Riphaty <riphatymkude008@gmail.com>
2026-04-28 19:29:40 +0300

    hosts

--- alamo/settings.py
+++ alamo/settings.py
@@ -31,9 +31,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # ===================================================================
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]                                  
+ALLOWED_HOSTS = ["alamodukani.com", "www.alamodukani.com"]                                  
 
 
 # Application definition
