d4d368487f4e5b5cb1ea2cb3a30178029424fa05
Riphaty <riphatymkude008@gmail.com>
2026-05-03 17:47:30 +0300

    location

--- alamo/settings.py
+++ alamo/settings.py
@@ -179,7 +179,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
 }
 
