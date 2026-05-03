7fa3b7937c5cbdc03354c3a0add6c1cfa234bb96
Riphaty <riphatymkude008@gmail.com>
2026-05-03 17:32:32 +0300

    location

--- alamo/settings.py
+++ alamo/settings.py
@@ -138,6 +138,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+STATIC_URL = 'static/'
 
 # MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
@@ -194,8 +195,4 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'admin_panel'
 LOGOUT_REDIRECT_URL = 'login'
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / 'staticfiles'
